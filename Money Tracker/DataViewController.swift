e61027a0a41a3fd9bc8cf928c3eea982a2ed97c7
drandicus <deverasd@gmail.com>
2014-10-18 11:47:44 -0500

    Initial Commit

---
+++ Money Tracker/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Money Tracker
+//
+//  Created by Diego Deveras on 10/18/14.
+//  Copyright (c) 2014 CS465-11. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
